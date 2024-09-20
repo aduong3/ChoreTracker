d5fa01a4716f7ef40b3fe456c48931bcada051fc
aduong32 <aduong32@ucmerced.edu>
2024-09-19 17:20:09 -0700

    started on front-end and added tailwind package

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,26 @@ import './App.css'
 
 function App() {
 
-
   return (
-    <>
+    <> 
+      <div id="main-container">
+        <div id="home-info">
+          <h2>Points: #</h2>
+          <h2>Amount of Chores: #</h2>
+        </div>
+
+        <div id="home-buttons">
+          <button>
+            <span>Add Chores</span>
+          </button>
+          <button>
+            <span>View Chores</span>
+          </button>
+          <button>
+            <span>Reward Shop</span>
+          </button>
+        </div>
+      </div>
     </>
   )
 };
