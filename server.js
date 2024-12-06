f4a3073dee8477081f50ad801e4b244c01cd5050
aduong32 <aduong32@ucmerced.edu>
2024-12-05 18:21:57 -0800

    removed trailing / in cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['https://householdhero.netlify.app/', "http://localhost:5173"],
+  origin: ['https://householdhero.netlify.app', "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
